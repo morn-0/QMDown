7660c657d74c509d7e98b927e258a56f575572b8
Luren <68656403+luren-dc@users.noreply.github.com>
2024-11-17 13:12:27 +0800

    feat: 支持 logging

--- QMDown/__init__.py
+++ QMDown/__init__.py
@@ -1,12 +1,31 @@
 import gettext
+import logging
 from pathlib import Path
 
 from rich.console import Console
+from rich.logging import RichHandler
 
 __version__ = "0.1.0"
 
 console = Console()
 
+logging.basicConfig(
+    level="INFO",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[
+        RichHandler(
+            show_time=True,
+            show_path=False,
+            markup=True,
+            rich_tracebacks=True,
+            console=console,
+        )
+    ],
+)
+
+logging.info(f"QMDown {__version__}")
+
+
 gettext.bindtextdomain("zh_CN", localedir=str(Path(__file__).parent / "languages"))
 gettext.textdomain("zh_CN")
-
